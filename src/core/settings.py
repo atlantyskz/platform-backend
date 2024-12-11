e4c156ecfdd494e0567d1e62d64d80251d04fbc2
Temirlan <temirlan.kazhigerey@gmail.com>
2024-12-11 17:26:56 +0500

    fix: settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -15,7 +15,12 @@ class Settings:
 
     @property
     def get_db_url(self):
-        return f"postgresql+asyncpg://postgres:postgres@localhost:5432/platform-db"
-
+        db_host = os.getenv('DB_HOST')
+        db_port = os.getenv('DB_PORT')
+        db_user = os.getenv('DB_USER')
+        db_password = os.getenv('DB_PASSWORD')
+        db_name = os.getenv('DB_NAME')
+        
+        return f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 settings = Settings()
     
