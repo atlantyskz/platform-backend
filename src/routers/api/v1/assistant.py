48a60945f842f967e0b7e15b6668fc832fb2c9be
Temirlan <temirlan.kazhigerey@gmail.com>
2025-02-06 16:46:28 +0500

    feat: update refund

--- src/routers/api/v1/assistant.py
+++ src/routers/api/v1/assistant.py
@@ -7,7 +7,7 @@ from src.core.factory import Factory
 assistant_router = APIRouter(prefix='/api/v1/assistants',tags=['ASSISTANT'])
 
 @assistant_router.post('/add_to_organization/{assistant_id}')
-@require_roles([RoleEnum.ADMIN])
+@require_roles([RoleEnum.ADMIN.value])
 async def add_assistant_to_organization(
     assistant_id: int,
     assistant_controller:AssistantController = Depends(Factory.get_assistant_controller),
@@ -16,7 +16,7 @@ async def add_assistant_to_organization(
     return await assistant_controller.add_assistant_to_organization(current_user.get('sub'),assistant_id)
 
 @assistant_router.delete('/delete_from_organization/{assistant_id}')
-@require_roles([RoleEnum.ADMIN])
+@require_roles([RoleEnum.ADMIN.value])
 async def delete_from_organization(
     assistant_id: int,
     assistant_controller:AssistantController = Depends(Factory.get_assistant_controller),
@@ -25,7 +25,7 @@ async def delete_from_organization(
     return await assistant_controller.delete_from_organization(current_user.get('sub'),assistant_id)
 
 @assistant_router.get('/get_all_assistants')
-@require_roles([RoleEnum.ADMIN,RoleEnum.EMPLOYER])
+@require_roles([RoleEnum.ADMIN.value,RoleEnum.EMPLOYER.value])
 async def get_all_assistants(
     assistant_controller: AssistantController = Depends(Factory.get_assistant_controller),
     current_user:dict = Depends(get_current_user)
