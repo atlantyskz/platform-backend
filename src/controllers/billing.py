42de263fd5d15dc8aa55c22560711f09bbbd1722
Temirlan <temirlan.kazhigerey@gmail.com>
2025-02-06 17:52:08 +0500

    fix: refund exceptions

--- src/controllers/billing.py
+++ src/controllers/billing.py
@@ -313,7 +313,7 @@ class BillingController:
             if transaction is None:
                 raise NotFoundException("Transaction not found")
             
-            if transaction.status != 'pending refound':
+            if transaction.status != 'pending refund':
                 raise BadRequestException("Transaction already refunded,pending or rejected")
             print({"retrieved_transaction": transaction})
 
