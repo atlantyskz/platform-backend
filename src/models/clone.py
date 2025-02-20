c56794a14a562d14f75b2deb3962bbf1083b0308
Temirlan <temirlan.kazhigerey@gmail.com>
2025-02-20 17:19:06 +0500

    fix hh

--- src/models/clone.py
+++ src/models/clone.py
@@ -11,7 +11,7 @@ class Clone(Base,TimestampMixin):
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('users.id',ondelete='CASCADE'))
     agreement_video_path: so.Mapped[str] = so.mapped_column(sa.String,nullable=False)
     sample_video_path: so.Mapped[str] = so.mapped_column(sa.String,nullable=False)
-    lipsynch_text:so.Mapped[str] = so.mapped_column(sa.Text(3000),nullable=False)
+    lipsynch_text:so.Mapped[str] = so.mapped_column(sa.String(3000),nullable=False)
     status: so.Mapped[str] = so.mapped_column(sa.String,default='pending')
 
 
