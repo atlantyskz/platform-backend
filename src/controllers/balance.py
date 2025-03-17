e1a8cfbb85ee84dcc753aca317ed93feddacf21e
dimash <arystambekdimash005@gmail.com>
2025-03-17 15:45:07 +0500

    upd: added free trial to response

--- src/controllers/balance.py
+++ src/controllers/balance.py
@@ -1,23 +1,22 @@
-from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.exceptions import NotFoundException
+from src.repositories.balance import BalanceRepository
 from src.repositories.balance_usage import BalanceUsageRepository
 from src.repositories.organization import OrganizationRepository
 from src.repositories.user import UserRepository
-from src.repositories.balance import BalanceRepository
-from src.core.exceptions import NotFoundException
 
 
 class BalanceController:
 
-    def __init__(self,session:AsyncSession):
+    def __init__(self, session: AsyncSession):
         self.session = session
         self.balance_repository = BalanceRepository(session)
         self.balance_usage_repository = BalanceUsageRepository(session)
         self.organization_repository = OrganizationRepository(session)
         self.user_repository = UserRepository(session)
-        
-    
-    async def get_balance(self,user_id:int):
+
+    async def get_balance(self, user_id: int):
         user = await self.user_repository.get_by_user_id(user_id)
         if user is None:
             raise NotFoundException("User not found")
@@ -25,16 +24,17 @@ class BalanceController:
         print(organization)
         balance = await self.balance_repository.get_balance(organization.id)
         if balance is None:
-            balance = await self.balance_repository.create_balance({"organization_id":organization.id,"atl_tokens":10})
-        return {"balance":round(balance.atl_tokens,2)}
-    
+            balance = await self.balance_repository.create_balance(
+                {"organization_id": organization.id, "atl_tokens": 10})
+        return {"balance": round(balance.atl_tokens, 2), "free_trial": balance.free_trial}
 
-    async def get_balance_usage(self,user_id, assistant_id, start_date, end_date, limit, offset):
+    async def get_balance_usage(self, user_id, assistant_id, start_date, end_date, limit, offset):
         user = await self.user_repository.get_by_user_id(user_id)
         if user is None:
             raise NotFoundException("User not found")
         organization = await self.organization_repository.get_user_organization(user_id)
-        balance_usage = await self.balance_usage_repository.get_balance_usage(user.id, organization.id, assistant_id, start_date, end_date, limit, offset)
+        balance_usage = await self.balance_usage_repository.get_balance_usage(user.id, organization.id, assistant_id,
+                                                                              start_date, end_date, limit, offset)
         return [
             {
                 "id": usage.id,
