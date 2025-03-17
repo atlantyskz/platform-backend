3e72816e8a1e54156fb5dae3eafe8cd4f6d485bc
dimash <arystambekdimash005@gmail.com>
2025-03-17 15:42:35 +0500

    upd: the format of created at and updated_at chagned to utcnow from utc now

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime
+
 from sqlalchemy.orm import declarative_base,mapped_column
 from sqlalchemy import DateTime, func
 from sqlalchemy.ext.declarative import declared_attr
@@ -6,22 +8,22 @@ from sqlalchemy.ext.declarative import declared_attr
 class TimestampMixin:
     @declared_attr
     def created_at(cls):
-        return mapped_column(DateTime, default=func.now(), nullable=False)
+        return mapped_column(DateTime, default=datetime.utcnow, nullable=False)
 
     @declared_attr
     def updated_at(cls):
         return mapped_column(
             DateTime,
-            default=func.now(),
-            onupdate=func.now(),
+            default=datetime.utcnow,
+            onupdate=datetime.utcnow,
             nullable=False,
         )
 
 
 Base = declarative_base()
 from .user import User
-from .permission import Permission,role_permissions
-from .role import Role,role_permissions
+from .permission import Permission, role_permissions
+from .role import Role, role_permissions
 from .organization import Organization
 from .assistant import Assistant
 from .organization_member import OrganizationMember
@@ -52,7 +54,7 @@ sql_admin_models_list = [
     FavoriteResume,
     VacancyRequirement,
     ChatMessageHistory,
-    UserFeedback,   
+    UserFeedback,
     BillingTransaction,
     Balance,
     Discount,
@@ -60,4 +62,3 @@ sql_admin_models_list = [
     RefundApplication,
     HHAccount,
 ]
-
