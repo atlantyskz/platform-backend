9861e4c106b1be92e6145d3a99df677ce2249879
dimash <arystambekdimash005@gmail.com>
2025-04-11 20:54:32 +0500

    feat: fixed resume ids

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -234,7 +234,7 @@ async def _generate_questions_for_resume(
             print(question, "\n\n\n")
             print("Question Text",question.get("question_text", ""))
             await question_repo.create_question({
-                "resume_id": resume_record.resume_id,
+                "resume_id": resume_record.id,
                 "question_text": question.get("question_text", "")
             })
             await session.flush()
