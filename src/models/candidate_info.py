1cdf5def3ebe2f189d139e3fc69a1da55671673f
dimash <arystambekdimash005@gmail.com>
2025-04-14 11:04:57 +0500

    feat: changed logic

---
+++ src/models/candidate_info.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+
+from src.models import Base
+
+
+class CandidateInfo(Base):
+    __tablename__ = 'candidate_info'
+
+    id: orm.Mapped[int] = orm.mapped_column(sa.Integer, primary_key=True)
+    candidate_info: orm.Mapped[str] = orm.mapped_column(sa.JSON, default={})
+    resume_url: orm.Mapped[str] = orm.mapped_column(sa.String)
